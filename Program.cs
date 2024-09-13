ee63a8476d0607506be67070a0b2a1ec19bde509
339713273 <339713273@2260I-L29-026>
2024-09-13 08:41:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, haircolor, weight, height, age ; 
+        }Age
+    }
+}
