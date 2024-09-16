16c9b24be4a801842e1ac45fdc052798cb135ef9
339713273 <339713273@2260I-L29-026>
2024-09-16 09:35:59 -0400

    Still working

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@
             string courseName = "Computer Science";
 
             Console.WriteLine("This is room #" + roomNumber);
+            Console.WriteLine("The price is " + price);
+            Console.WriteLine("I am leaning a bit about " + courseName);
+
+            string fullName;
+            int gradYear; 
+
+            fullName = "Carter Todd";
+            gradYear = 2026;
+
+            Console.WriteLine("My name is " + fullName + " and i'll graduate in " + gradYear);
+
+            int radius;
+            double area; 
+            radius = 5;
+            area = 5;
+            Console.WriteLine(area = Math.PI * (radius * 2));
         }
     }
 }
